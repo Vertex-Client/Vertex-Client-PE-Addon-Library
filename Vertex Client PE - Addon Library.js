959ddd3e38e748ec436ab7365a876939bc9368a4
Vertex Client <rubenrubentje@hotmail.nl>
2016-06-25 14:11:58 +0200

    Create Vertex Client PE - Addon Library.js

---
+++ Vertex Client PE - Addon Library.js
@@ -0,0 +1,161 @@
+/**
+*  Library made by peacestorm
+*  © 2016
+*/
+
+/**
+* PART 1 OF THE FUNCTION LIBRARY (DON'T EDIT)
+*/
+
+var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+
+var Launcher = {
+	isBlockLauncher: function() {
+		return (ctx.getPackageName() == "net.zhuoweizhang.mcpelauncher" || ctx.getPackageName() == "net.zhuoweizhang.mcpelauncher.pro");
+	},
+	isToolbox: function() {
+		return ctx.getPackageName() == "io.mrarm.mctoolbox";
+	},
+	isMcpeMaster: function() {
+		return ctx.getPackageName() == "com.mcbox.pesdkb.mcpelauncher";
+	}
+};
+
+/**
+* YOUR ADDON CONTENT
+*/
+
+const ADDON_NAME = "Example addon"; //Your addon's name
+const ADDON_DESC = "Adds example modules into Vertex Client PE."; //Your addon's description
+const ADDON_VERSION = "1.0"; //Your addon's version
+const TARGET_VERSION = "1.0"; //Your addon's target Vertex Client PE version (in this case we use Vertex Client PE v1.0)
+
+var modules = [];
+
+const Category = {
+	COMBAT: 0,
+	BUILDING: 1,
+	MOVEMENT: 2,
+	CHAT: 3,
+	MISC: 4
+}
+
+/**
+ *	CUSTOM MODULES
+ */
+
+/*
+var exampleModule = {
+	name: "Example toggleable module",
+	desc: "Example description.",
+	category: Category.MOVEMENT,
+	type: "Mod",
+	state: false,
+	isStateMod: function() {
+		return true;
+	},
+	onToggle: function() {
+		this.state = !this.state;
+		if(this.state) {
+			//do something when this module is enabled
+		} else {
+			//do something when this module is disabled
+		}
+	},
+	onTick: function() {
+		//this will get called in modTick when the mod is enabled, also possible: onUseItem, onAttack, onHurt, onChat etcetera (add these functions separated by a comma, like what I did with onUseItem in this module)
+	},
+	onUseItem: function(x, y, z, itemId, blockId, side, blockDamage) {
+		//add your useItem code for this mod here
+	}
+};
+
+registerModule(exampleModule);
+
+***************************************************************************************************************
+
+Other parameters you can add to your modules are the following:
+
+getSettingsLayout: function() {
+  var settingsLayout = new LinearLayout(ctx);
+  //add extra widgets (such as buttons and texts) in here, they will be displayed in a mod's ... dialog
+  return settingsLayout;
+},
+onModDialogDismiss: function() {
+  //this will be called when an user closes a mod's ... dialog
+},
+canBypassYesCheatPlus: function() {
+  return false; //if the mod should be blocked by YesCheat+
+}
+
+If you want to store a variable in a module, simply add it like this: yourVariableName: yourVariableValue just like other parameters.
+You can call this variable from within the module using this.yourVariableName.
+
+It's still possible to use normal vars within the module's functions (onUseItem, onTick etcetera)
+
+***************************************************************************************************************
+
+var secondExampleModule = {
+	name: "Example non-toggleable module",
+	desc: "Example description.",
+	category: Category.COMBAT,
+	type: "Mod",
+	isStateMod: function() {
+		return false;
+	},
+	onToggle: function() {
+		//do something when an user taps on this mod's button
+	}
+};
+
+registerModule(secondExampleModule);
+
+***************************************************************************************************************
+Other functions and variables you can use in your addon are the following:
+##################################
+callVertexFunction(functionName, propArray);
+>> Example: callVertexFunction("nuker", [exampleX, exampleY, exampleZ, 3, "cube"]); <<
+^^ In this example it will call VertexClientPE.nuker without having to copy the whole function into your addon. This also works for other functions. ^^
+----------------------------------
+Launcher.isBlockLauncher();
+>> Example: Launcher.isBlockLauncher(); <<
+^^ This will return true if the user uses BlockLauncher. If not, it will return false.
+----------------------------------
+Launcher.isToolbox();
+>> Example: Launcher.isToolbox(); <<
+^^ This will return true if the user uses the Toolbox launcher. If not, it will return false.
+----------------------------------
+Launcher.isMcpeMaster();
+>> Example: Launcher.isMcpeMaster(); <<
+^^ This will return true if the user uses the MCPE Master launcher. If not, it will return false.
+##################################
+***************************************************************************************************************
+
+*/
+
+/**
+ *	PART 2 OF THE FUNCTION LIBRARY (DON'T EDIT)
+ */
+
+function addonLoadHook() {
+  if(Launcher.isBlockLauncher() || Launcher.isMcpeMaster()) {
+    net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod("registerAddon", [ADDON_NAME, ADDON_DESC, ADDON_VERSION, TARGET_VERSION, modules]);
+  }
+  if(Launcher.isMcpeMaster()) {
+		com.mcbox.pesdk.mcpelauncher.ScriptManager.callScriptMethod("registerAddon", [ADDON_NAME, ADDON_DESC, ADDON_VERSION, TARGET_VERSION, modules]);
+	}
+}
+
+function registerModule(obj) {
+	obj.source = ADDON_NAME;
+	modules.push(obj);
+}
+
+function callVertexFunction(functionName, propArray) {
+  if(Launcher.isBlockLauncher() || Launcher.isMcpeMaster()) {
+  	net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod("VertexClientPE." + functionName, propArray);
+  }
+  if(Launcher.isMcpeMaster()) {
+		com.mcbox.pesdk.mcpelauncher.ScriptManager.callScriptMethod("VertexClientPE." + functionName, propArray);
+	}
+}
